890fccb0ef15958a3c99b11fcfb666123b8d2c5b
karnauh.vitaliy <karnauhvitaliy@gmail.com>
2015-10-26 23:27:11 +0200

    Add Hibernate

---
+++ src/main/java/com/checkdoc/config/DBConfig.java
@@ -0,0 +1,76 @@
+package com.checkdoc.config;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate4.HibernateTemplate;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@PropertySource("classpath:application.properties")
+public class DBConfig {
+
+
+    @Inject
+    private Environment environment;
+
+    private Properties getHibernateProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", environment.getProperty("hibernate.dialect"));
+        properties.setProperty("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
+        properties.setProperty("hibernate.hbm2ddl.auto",
+                environment.getProperty("hibernate.hbm2ddl.auto"));
+        return properties;
+    }
+
+    @Autowired
+    @Bean(name = "hibernateTemplate")
+    @Scope(WebApplicationContext.SCOPE_REQUEST)
+    public HibernateTemplate getHibernateTemplate(SessionFactory sessionFactory) {
+        return new HibernateTemplate(sessionFactory);
+    }
+
+
+    @Autowired
+    @Bean(name = "sessionFactory")
+    public SessionFactory getSessionFactory(DataSource dataSource) {
+        LocalSessionFactoryBuilder localSessionFactoryBuilder = new LocalSessionFactoryBuilder(
+                dataSource);
+        localSessionFactoryBuilder.addProperties(getHibernateProperties());
+        localSessionFactoryBuilder.scanPackages("com.checkdoc.domain");
+        return localSessionFactoryBuilder.buildSessionFactory();
+    }
+
+    @Bean(name = "dataSource")
+    public DataSource getDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(environment.getProperty("driverClassName"));
+        dataSource.setUrl(environment.getProperty("url"));
+        dataSource.setUsername(environment.getProperty("username"));
+        dataSource.setPassword(environment.getProperty("password"));
+        return dataSource;
+    }
+
+    @Autowired
+    @Bean(name = "transactionManager")
+    @Scope(WebApplicationContext.SCOPE_REQUEST)
+    public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager hibernateTransactionManager = new HibernateTransactionManager();
+        hibernateTransactionManager.setSessionFactory(sessionFactory);
+        return hibernateTransactionManager;
+    }
+}
+
