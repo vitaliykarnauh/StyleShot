2c40b644068943366666c7bfbe0bbb67eeda3546
Mary Solodovnik <gimiro42@gmail.com>
2015-11-27 00:53:19 +0200

    changed upload area

---
+++ src/main/resources/META-INF/resources/js/script.js
@@ -0,0 +1,65 @@
+$(document).ready(function (){
+	'use scrict'
+
+
+	// $('[data-toggle="tooltip"]').tooltip({
+    //     placement: 'top'
+    // });
+
+    //$("#header").load("header.html");
+    $('.error-div').hide();
+
+    $('#file-upload').change(function() {
+    	$('#upload-btn').prop('disabled', false);
+    	$full_path = $('#file-upload').val();
+    	partials = $full_path.split('\\');
+		$file_name = partials[partials.length - 1];
+		extension = $file_name.split('\.').slice(-1)[0];
+		if (extension != 'docx') {
+			showExtError();
+		} else {
+			everythingIsFine($file_name);
+		}
+    });
+
+    Dropzone.options.filesdropzone = {
+		paramName: "file", // The name that will be used to transfer the file
+		accept: function(file, done) {
+			if (file.name.split('\.').slice(-1)[0] != 'docx') {
+				// alert(file.type);
+				this.removeFile(file);
+				showExtError();
+			} else {
+				done();
+				everythingIsFine();
+			}
+			// alert(file);
+			// extension = file.name.split('\.').slice(-1)[0];
+			// if (extension != "docx") {
+			// 	showExtError();
+			// }
+			// else { 
+			// 	everythingIsFine();
+			// }
+		},
+		maxFiles: 1
+	};
+});
+
+
+function showExtError() {
+	// $('#upload-btn').prop('disabled', true);
+	// $('.file-upload-name').val('');
+	$('.error-div').fadeIn(600);
+}
+
+function everythingIsFine($file_name) {
+	// $('.file-upload-name').val($file_name);
+    $('.error-div').fadeOut(600);
+}
+
+$(window).load(function() {
+	$dz_div = $('div.dz-default.dz-message');
+	$dz_div.find('span').html('Перетащите файл для загрузки');
+	$dz_div.append('<img src="../META-INF/resources/images/inbox-upload.png" alt="uploading pic" class="uploading-pic" />');
+});
